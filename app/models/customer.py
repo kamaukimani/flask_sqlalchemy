9702e0e61b31ac2c35eaa800ea3c7988fcf1cd4b
kamaukimani <paulkimys285@gmail.com>
2026-09-11 16:17:35 +0300

    add SerializerMixin and serializer_rules

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,9 +2,12 @@ from app.db import db
 from sqlalchemy.orm import Mapped,mapped_column,relationship
 from typing import List
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
 
-class Customer(db.Model):
+class Customer(db.Model,SerializerMixin):
     __tablename__="customers"
+
+    serialize_rules=("-reviews.customer",)
 
     id:Mapped[int]=mapped_column(primary_key=True)
     name:Mapped[str]
